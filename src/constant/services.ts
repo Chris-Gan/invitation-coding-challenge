670d531fc934fb60901cf29bb85c8e40e4440b6b
Chris <chris.gan0831@gmail.com>
2023-07-12 22:01:31 +1000

    update API endpoint

--- src/constant/services.ts
+++ src/constant/services.ts
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export const endpoint = process.env.MOCKED_API;
+export const endpoint = 'https://invitation-registration-mocked-2860f0b97211.herokuapp.com/register';
